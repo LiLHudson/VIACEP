86bc11d53010bc001447c4098597a43e8de9c18b
Hudson Alves <136837521+LiLHudson@users.noreply.github.com>
2023-06-20 07:15:45 -0300

    Add files via upload

--- viacep.js
+++ viacep.js
@@ -5,24 +5,26 @@ const cidade = document.querySelector('#cidade');
 const estado = document.querySelector('#estado');
 const numero = document.querySelector('#numero');
 const message = document.querySelector('#message');
+const cepLenght = document.querySelector('tamanho');
 
-cep.addEventListener('focusout', async() => {
 
+cep.addEventListener('focusout',async() => {
         try {
-            const cepValid = /^[0-9]{5}[\d]{3}$/
-	    teste = cep.value.replace("-","");
-            cep.value = teste;
+            const cepValid = /^[0-9]{5}-[\d]{3}$/
 
         if (!cepValid.test(cep.value)) {
             throw { cep_error: 'Cep Invalido'};
             }
-            const response = await fetch(`https://viacep.com.br/ws/${teste}/json/`);
+            const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
             if(!response.ok){
                 throw await response.json();
             }
 	    
             const responseCep = await response.json();
-		
+            if(responseCep.erro){
+                throw{cep_error: 'Cep Invalido'};
+            }
+            
             address.value = responseCep.logradouro;
             bairro.value = responseCep.bairro;
             cidade.value = responseCep.localidade;
@@ -39,3 +41,9 @@ cep.addEventListener('focusout', async() => {
         }    
         
 })
+function limparForm(){
+    document.querySelector('#address').value = "";
+    document.querySelector('#cidade').value = "";
+    document.querySelector('#bairro').value = "";
+    document.querySelector('#estado').value = "";
+}
